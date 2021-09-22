2a85d249fcb8482a796fdad91071a7db454d3cc5
boh <hosakbenoliver@outlook.de>
2021-09-22 21:13:42 +0200

    added basic CLI input functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,60 @@
+use std::env;
+use std::error::Error;
+
 pub struct Bewohner{
 
     pub id: i32,
     pub name: String,
     // Gebutstag: DATE
     pub admin: bool,
+    pub username: String,
+    pub passwort: String,
     
 
 }
 
 impl Bewohner{
-    pub fn new(id: i32, name: String, admin: bool) -> Bewohner{
+    pub fn new(id: i32, name: String, admin: bool,username: String, passwort: String) -> Bewohner{
        Bewohner{
         id: id,
         name: name,
         // Gebutstag: DATE
         admin: admin,
+        username: username,
+        passwort: passwort,
        }  
     }
+}
+
+pub struct Befehl{
+    pub high_level: String,
+    pub low_level: String,
+}
+
+impl Befehl{
+    pub fn new(mut args: env::Args) -> Result<Befehl, &'static str>{
+        //TODO Fehlermeldungen
+
+        args.next();
+
+        let high_level = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not find high level command"),
+        };
+
+        let low_level = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not find low level command"),
+        };
+
+
+        Ok(Befehl {
+            high_level,
+            low_level,
+        })
+    }
+}
+
+pub fn interp(befehl: Befehl){
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use std::env;
+use std::process;
+
 use wg_verwaltung::Bewohner;
+use wg_verwaltung::Befehl;
+
 
 fn main() {
 
-    let admin = Bewohner::new(0, String::from("Tobias"), true);
-    print!("{}", admin.name);
+
+    let befehl = Befehl::new(env::args()).unwrap_or_else(|err|{
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    wg_verwaltung::interp(befehl);
+
+    
 
 }
